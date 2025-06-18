336d06dc371f4b791335192f1bbfb6b3b27f61bc
odnods <lethalmc@gmail.com>
2025-06-18 17:35:01 -0300

    format

--- apps/front-end/src/components/sections/Footer/Footer.tsx
+++ apps/front-end/src/components/sections/Footer/Footer.tsx
@@ -5,15 +5,33 @@ export const Footer = (): ReactElement => (
   <footer>
     <div className="left">
       <ul className="footer-links">
-        <li><a href="/bridge">Bridge USDC</a></li>
-        <li><a href="https://docs.stable.io/">Docs</a></li>
-        <li><a href="https://github.com/stable-io/stable" target="_blank">Github</a></li>
+        <li>
+          <a href="/bridge">Bridge USDC</a>
+        </li>
+        <li>
+          <a href="https://docs.stable.io/">Docs</a>
+        </li>
+        <li>
+          <a href="https://github.com/stable-io/stable" target="_blank">
+            Github
+          </a>
+        </li>
       </ul>
       <ul className="footer-links">
-        <li><a href="https://blog.stable.io">Why Stable</a></li>
-        <li><a href="https://blog.stable.io">Blog</a></li>
-        <li><a href="https://x.com/stable_io" target="_blank">Twitter</a></li>
-        <li><a href="mailto:hello@stable.io">Reach out</a></li>
+        <li>
+          <a href="https://blog.stable.io">Why Stable</a>
+        </li>
+        <li>
+          <a href="https://blog.stable.io">Blog</a>
+        </li>
+        <li>
+          <a href="https://x.com/stable_io" target="_blank">
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a href="mailto:hello@stable.io">Reach out</a>
+        </li>
       </ul>
     </div>
     <div className="right">
--- apps/front-end/src/pages/index.tsx
+++ apps/front-end/src/pages/index.tsx
@@ -12,107 +12,144 @@ const Home: NextPageWithLayout = (): ReactElement => (
         Stable | Move USDC across networks with high speed and minimal costs
       </title>
     </Head>
-			<div className="hero">
-				<div className="left">
-					<h1 style={{ color: "#29233C"}}>Move <span style={{color: "#7667EC"}}>USDC</span> across chains. Blazing <span style={{color: "#48C6DA"}}>FAST!</span></h1>
-					<h2>Powered by Circle, Stable's SDK enables instant USDC transfers across networks.</h2>
-					<a href="/bridge" className="bridge-link"><button className="main-cta">Bridge Now</button></a>
-					<a href="https://docs.stable.io" className="docs-link" style={{marginLeft: "20px"}}><button className="text-cta">Start building →</button></a>
-				</div>
-				<div className="right transfer-widget-placeholder-container">
-					<a href="/bridge">
-						<div className="widget-placeholder">
-							<Image
-                unoptimized
-                width={500}
-                height={623}
-                alt="transfer-widget"
-                src="/imgs/transfer-widget-placeholder.png"
-              />
-							<div className="placeholder-overlay"></div>
-						</div>
-						
-					</a>
-				</div>
-			</div>
-			<div className="offerings">
-				<ul>
-					<li>
-						<Image
-              src="/imgs/radial-circle-0.png"
-              className="icon"
+    <div className="hero">
+      <div className="left">
+        <h1 style={{ color: "#29233C" }}>
+          Move <span style={{ color: "#7667EC" }}>USDC</span> across chains.
+          Blazing <span style={{ color: "#48C6DA" }}>FAST!</span>
+        </h1>
+        <h2>
+          Powered by Circle, Stable's SDK enables instant USDC transfers across
+          networks.
+        </h2>
+        <a href="/bridge" className="bridge-link">
+          <button className="main-cta">Bridge Now</button>
+        </a>
+        <a
+          href="https://docs.stable.io"
+          className="docs-link"
+          style={{ marginLeft: "20px" }}
+        >
+          <button className="text-cta">Start building →</button>
+        </a>
+      </div>
+      <div className="right transfer-widget-placeholder-container">
+        <a href="/bridge">
+          <div className="widget-placeholder">
+            <Image
               unoptimized
-              width={64}
-              height={64}
-              alt="step-0-image"
+              width={500}
+              height={623}
+              alt="transfer-widget"
+              src="/imgs/transfer-widget-placeholder.png"
             />
-						<h3>Fast & cost-efficient USDC transfers</h3>
-						<p>Seamlessly move USDC across networks with high speed and minimal costs.</p>
-					</li>
-					<li>
-						<Image
-              src="/imgs/radial-circle-1.png"
-              className="icon"
-              unoptimized
-              width={64}
-              height={64}
-              alt="step-1-image"
-            />
-						<h3>Built for developers, designed for scale</h3>
-						<p>A developer-friendly SDK designed for a smooth, intuitive integration experience.</p>
-					</li>
-					<li>
-						<Image
-              src="/imgs/radial-circle-2.png"
-              className="icon"
-              unoptimized
-              width={64}
-              height={64}
-              alt="step-2-image"
-            />
-						<h3>All the features you need and more..</h3>
-						<p>Mainnet and testnet support, gas drop-off, live transfer tracking, and a lot more!</p>
-					</li>
-				</ul>
-			</div>
+            <div className="placeholder-overlay"></div>
+          </div>
+        </a>
+      </div>
+    </div>
+    <div className="offerings">
+      <ul>
+        <li>
+          <Image
+            src="/imgs/radial-circle-0.png"
+            className="icon"
+            unoptimized
+            width={64}
+            height={64}
+            alt="step-0-image"
+          />
+          <h3>Fast & cost-efficient USDC transfers</h3>
+          <p>
+            Seamlessly move USDC across networks with high speed and minimal
+            costs.
+          </p>
+        </li>
+        <li>
+          <Image
+            src="/imgs/radial-circle-1.png"
+            className="icon"
+            unoptimized
+            width={64}
+            height={64}
+            alt="step-1-image"
+          />
+          <h3>Built for developers, designed for scale</h3>
+          <p>
+            A developer-friendly SDK designed for a smooth, intuitive
+            integration experience.
+          </p>
+        </li>
+        <li>
+          <Image
+            src="/imgs/radial-circle-2.png"
+            className="icon"
+            unoptimized
+            width={64}
+            height={64}
+            alt="step-2-image"
+          />
+          <h3>All the features you need and more..</h3>
+          <p>
+            Mainnet and testnet support, gas drop-off, live transfer tracking,
+            and a lot more!
+          </p>
+        </li>
+      </ul>
+    </div>
 
-			<div className="sdk-steps">
-				<h2>The SDK to Move USDC in 1-2-3</h2>
-				<div className="steps-container">
-					<ul className="steps">
-						<li className="step">
-							<div className="icon">
-								<span>1</span>
-							</div>
-							<div className="details">
-								<div className="step-title">Initialize The SDK</div>
-								<div className="step-desc">Create a StableSDK instance by providing a network, signer, and optional RPCs.</div>
-							</div>
-						</li>
-						<li className="step step-follow"><div className="icon"></div></li>
-						<li className="step">
-							<div className="icon">
-								<span>2</span>
-							</div>
-							<div className="details">
-								<div className="step-title">Find Routes</div>
-								<div className="step-desc">Provide the source/target chains, amount, and addresses to get available transfer routes.</div>
-							</div>
-						</li>
-						<li className="step step-follow"><div className="icon"></div></li>
-						<li className="step">
-							<div className="icon" style={{ border: "0px"}}>
-								<span>🎉</span>
-							</div>
-							<div className="details">
-								<div className="step-title">Execute The Transfer</div>
-								<div className="step-desc">Execute the route to sign and submit all required transactions. It’s that simple!</div>
-							</div>
-						</li>
-					</ul>
-					<a href="https://docs.stable.io/"><button className="main-cta">Start building →</button></a>
-				</div>
-			</div>
+    <div className="sdk-steps">
+      <h2>The SDK to Move USDC in 1-2-3</h2>
+      <div className="steps-container">
+        <ul className="steps">
+          <li className="step">
+            <div className="icon">
+              <span>1</span>
+            </div>
+            <div className="details">
+              <div className="step-title">Initialize The SDK</div>
+              <div className="step-desc">
+                Create a StableSDK instance by providing a network, signer, and
+                optional RPCs.
+              </div>
+            </div>
+          </li>
+          <li className="step step-follow">
+            <div className="icon"></div>
+          </li>
+          <li className="step">
+            <div className="icon">
+              <span>2</span>
+            </div>
+            <div className="details">
+              <div className="step-title">Find Routes</div>
+              <div className="step-desc">
+                Provide the source/target chains, amount, and addresses to get
+                available transfer routes.
+              </div>
+            </div>
+          </li>
+          <li className="step step-follow">
+            <div className="icon"></div>
+          </li>
+          <li className="step">
+            <div className="icon" style={{ border: "0px" }}>
+              <span>🎉</span>
+            </div>
+            <div className="details">
+              <div className="step-title">Execute The Transfer</div>
+              <div className="step-desc">
+                Execute the route to sign and submit all required transactions.
+                It’s that simple!
+              </div>
+            </div>
+          </li>
+        </ul>
+        <a href="https://docs.stable.io/">
+          <button className="main-cta">Start building →</button>
+        </a>
+      </div>
+    </div>
   </>
 );
 
