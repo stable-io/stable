a01ca3e73af14ca36d37b4c0e68991221c3e92aa
horsecobbler <211329048+horsecobbler@users.noreply.github.com>
2025-06-23 12:42:55 +0100

    Reduce gas estimate in executeRoute (#51)

--- packages/stable-sdk/src/methods/executeRoute/executeRouteSteps.ts
+++ packages/stable-sdk/src/methods/executeRoute/executeRouteSteps.ts
@@ -97,7 +97,7 @@ function buildEvmTxParameters(tx: ContractTx, chain: ViemChain, account: ViemAcc
      * @todo: Proper gas calculation will be necessary at some point...
      *        we could consider using the gasEstimation field of the corresponding step.
      */
-    gas: fromGwei(0.01),
+    gas: fromGwei(0.001),
     maxFeePerGas: fromGwei(40),
     maxPriorityFeePerGas: fromGwei(20),
   };
