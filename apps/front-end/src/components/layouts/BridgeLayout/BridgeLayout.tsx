489d26fbaaa3ae69050551542e23d8b3ab53827d
odnods <lethalmc@gmail.com>
2025-07-07 17:51:15 -0300

    adds announcement banner to bridge page

--- apps/front-end/src/components/layouts/BridgeLayout/BridgeLayout.tsx
+++ apps/front-end/src/components/layouts/BridgeLayout/BridgeLayout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren, ReactElement } from "react";
-
+import { Announcement } from "@/components/sections";
 import { Background } from "../../elements/Background";
 import { Footer } from "../../sections/Footer";
 import { Header } from "../../sections/Header";
@@ -9,6 +9,7 @@ export type BridgeLayoutProps = PropsWithChildren<object>;
 export const BridgeLayout = ({ children }: BridgeLayoutProps): ReactElement => (
   <div className="bridge-app main">
     <Background />
+    <Announcement />
     <div className="container">
       <Header />
       <div className="main-content">{children}</div>
