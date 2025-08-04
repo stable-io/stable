a25aa704f8acd93e894c61acccb31af30deddc01
NoiTaTuM <noit63@gmail.com>
2025-08-04 18:02:27 -0300

    Small fixes to deployment configuration and scripts

--- deployment/evm/cctpr/verifyBytecode.sh
+++ deployment/evm/cctpr/verifyBytecode.sh
@@ -19,7 +19,7 @@ fi
 echo "Using environment: $ENV"
 echo ""
 
-yarnpkg tsx verifyBytecode.ts 2>/dev/null | while read -r line
+yarn tsx verifyBytecode.ts 2>/dev/null | while read -r line
 do
    cd ../../../contracts/cctpr/evm
    echo "Executing: $line"
